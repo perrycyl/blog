ddb4826e626879737aadc5e22b4991f9b1a1493f
Perry <perry@jamdirect.ca>
2020-12-30 12:24:13 -0500

    Added comment moderation demo. Added sync events when a service goes down.

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ({comments}) => {
-    const renderedComments = comments.map(comment =>{
-        return <li key={comment.id}>{comment.content}</li>
+export default ({ comments }) => {
+    const renderedComments = comments.map(comment => {
+        let content;
+
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+
+        if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation';
+        }
+
+        if (comment.status === 'rejected') {
+            content = 'This comment is rejected';
+        }
+
+        return <li key = { comment.id } > { content } </li>
     });
 
-    return (
-        <ul>{renderedComments}</ul>
+    return ( <ul> { renderedComments } </ul>
     )
 }
--- query/index.js
+++ query/index.js
@@ -1,39 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {}
-    /* sample
 
-    posts ==={
-        "jlaskdf": {
-            id: 'jlaskdf',
-            title: 'post title',
-            comments: [
-                {id:'asdfsd', content:'comment!'}
-            ]
-        },
-        "jlaskdf": {
-            id: 'jlaskdf',
-            title: 'post title',
-            comments: [
-                {id:'asdfsd', content:'comment!'}
-            ]
-        }
-    }
-
-    */
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
@@ -55,13 +31,30 @@ app.post('/events', (req, res) => {
 
         comment.status = status;
         comment.content = content;
-
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     console.log("\n---\n", posts)
     res.send({})
 });
 
-app.listen(4002, () => {
+app.listen(4002, async() => {
     console.log('Listening on 4002')
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        console.log('Processing event:', event.type);
+
+        handleEvent(event.type, event.data);
+    }
 });
